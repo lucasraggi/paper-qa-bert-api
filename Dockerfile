bf448751145de460ec0788b5e9711f4cb940a4b8
Lucas Raggi <lrr@ic.ufal.br>
2020-02-28 04:27:48 -0300

    fix extract text from pdf using tinka

--- Dockerfile
+++ Dockerfile
@@ -41,5 +41,4 @@ RUN python3 -m pip install --upgrade pip
 RUN pip3 install -r requirements.txt
 RUN python3 -m deeppavlov install squad_bert
 
-
 CMD python3 paper-qa-api.py
--- paper-qa-api.py
+++ paper-qa-api.py
@@ -1,3 +1,4 @@
+# coding=utf8
 from deeppavlov import build_model, configs
 from werkzeug.utils import secure_filename
 from flask import Flask
@@ -5,10 +6,13 @@ from flask import request
 from flask import jsonify
 from flask import redirect
 from flask_cors import CORS
+from tika import parser
+from waitress import serve
 import json
-from pdfminer import high_level
 import os
 import time
+import re
+
 
 UPLOAD_FOLDER = 'files/'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -51,16 +55,32 @@ def ask_paper():
         json_string = request.get_json()
         data_dump = json.dumps(json_string)
         data = json.loads(data_dump)
-        answer = {'answer': model([data.get('paper_text')], [data.get('question')])[0]}
-        response = jsonify(answer)
+        answer = model([data.get('paper')], [data.get('question')])[0]
+        if not answer:
+            answer = 'i dont know how to answer that, can you ask another question?'
+        answer_json = {'answer': answer}
+        response = jsonify(answer_json)
         return response
 
 
 def extract_text_from_pdf(file_path):
-    text = high_level.extract_text(file_path)
+    raw = parser.from_file(file_path)
+    text = raw['content'].encode('ascii', 'ignore')
+    text = text.decode('utf-8')
     one_line_text = str.join(" ", text.splitlines())
     one_line_text = one_line_text.replace('-', '')
+    one_line_text = clean_pdf_text(one_line_text)
     return one_line_text
+
+
+def clean_pdf_text(pdf_text):
+    pdf_text = pdf_text.lower()
+    pdf_text = pdf_text.split("abstract", 1)[1]
+    pdf_text = pdf_text.split("references", 1)[0]
+    pdf_text = pdf_text.replace(r"\(.*\)", "")
+    pdf_text = re.sub(r'http\S+', '', pdf_text)
+    pdf_text = re.sub("[\(\[].*?[\)\]]", "", pdf_text)
+    return pdf_text
 
 
 def setup_model():
@@ -73,7 +93,9 @@ def setup_model():
 
 def main():
     setup_model()
-    app.run(debug=False)
+    # extract_text_from_pdf('599.pdf')
+    serve(app, host='0.0.0.0', port=5000)
+    # app.run(debug=False, host='0.0.0.0')
 
 
 if __name__ == '__main__':
